32d37a31e4c16f60e88199a932494f83eea929dd
Soham <sohamdas273@gmail.com>
2021-09-20 21:38:32 +0530

    react-app deployment

---
+++ src/components/Tagline.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Tagline = () => (
+  <div className="tagline">
+    <p style={{fontWeight:"bold"}}>Enter The Name Of The City To Search</p>
+  </div>
+)
+
+export default Tagline
